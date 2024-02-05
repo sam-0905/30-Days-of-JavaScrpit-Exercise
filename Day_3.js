3db98a9e20e50a7d822521bda464d5c72a22bfb8
AllanSam <allansam0905@gmail.com>
2024-02-05 19:41:24 +0530

    complete Exercises: Level 1

--- Day_3.js
+++ Day_3.js
@@ -1,33 +1,86 @@
-console.log(hello);
+//console.log(hello);
 
 /*
 //Exercises: Level 1
-Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
-
+1.Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
 Enter your age: 30
 You are old enough to drive.
-
 Enter your age:15
 You are left with 3 years to drive.
-Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
 
+
+2.Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
 Enter your age: 30
 You are 5 years older than me.
-If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
 
-using if else
-ternary operator.
+3.If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
+-using if else
+-ternary operator.
   let a = 4
   let b = 3
   4 is greater than 3
-Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript?
 
+4.Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript?
 Enter a number: 2
 2 is an even number
 
 Enter a number: 9
 9 is is an odd number.
 **/
+
+//let userAge = prompt("Enter your age :  ");
+
+function checkUserAge() {
+  if (userAge >= 18) {
+    return "You are old enough to drive.";
+  } else {
+    return `You are left with ${18 - userAge} years to drive.`;
+  }
+}
+
+//console.log(checkUserAge());
+
+//let yourAge = prompt("Enter your age :  ");
+let myAge = 23;
+
+function checkAgeDiff() {
+  if (yourAge > myAge) {
+    return `You are ${yourAge - myAge} years older than me.`;
+  } else {
+    return `I am ${myAge - yourAge} years older than you  .`;
+  }
+}
+
+//console.log(checkAgeDiff());
+
+let a = 4;
+let b = 10;
+
+//let checkAOrB = checkAOrB ? a > b : console.log(a) ? b > a : console.log(b);
+
+function checkNum() {
+  if (a > b) {
+    return `${a} is greater than ${b}`;
+  } else {
+    return `${b} is greater than ${a}`;
+  }
+}
+
+console.log(checkNum());
+//console.log(checkAOrB);
+
+let number = prompt(`Enter a number`);
+console.log(number);
+
+function checkEvenOrOdd() {
+  if (number % 2 === 0) {
+    return `${number} it is even number`;
+  } else {
+    return `${number} it is odd number`;
+  }
+}
+
+console.log(checkEvenOrOdd());
 
 /*
 //Exercises: Level 2
