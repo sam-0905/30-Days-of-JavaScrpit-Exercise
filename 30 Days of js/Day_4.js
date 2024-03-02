39d0f55d853ac2e158a53ab409b98dfd01ad7893
AllanSam <allansam0905@gmail.com>
2024-03-02 09:06:21 +0530

    completed array exersice

--- 30 Days of js/Day_4.js
+++ 30 Days of js/Day_4.js
@@ -30,6 +30,8 @@ Remove the last IT company from the array
 Remove all IT companies
 **/
 
+//const newCountries = require("./countries");
+
 const countries = [
   "Albania",
   "Bolivia",
@@ -176,3 +178,317 @@ console.log(numbers.reverse());
 //splice -The Array.splice() method adds and removes array elements:
 const remAllEle = sentence.splice();
 console.log(remAllEle);
+
+/*
+Exercise: Level 2
+**/
+//console.log(newCountries.length);
+
+//1.First remove all the punctuations and change the string to array and count the number of words in the array
+
+let text =
+  "I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.";
+
+const words = text.split(" ");
+
+console.log(words);
+console.log(words.length);
+
+//2.In the following shopping cart add, remove, edit items
+
+//const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+//add 'Meat' in the beginning of your shopping cart if it has not been already added
+//add Sugar at the end of you shopping cart if it has not been already added
+//remove 'Honey' if you are allergic to honey
+//modify Tea to 'Green Tea'
+
+let shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
+
+console.log("unshift", shoppingCart.unshift("Meat"), shoppingCart);
+console.log("concat", shoppingCart.concat("Sugar"));
+console.log("remove", shoppingCart);
+
+let items = ["Milk", "Coffee", "Tea", "Honey", "Bread", "Meat"];
+
+items.slice(0, 3);
+items.slice(0, 3); //['Milk', 'Coffee', 'Tea']
+items.slice(4); //['Bread', 'Meat']
+items = [...items.slice(0, 3), ...items.slice(4)];
+
+function checkCountry() {
+  if (countries.includes("Ethiopia")) {
+    console.log(countries);
+  } else {
+    //console.log(...countries, "Ethiopia");
+    console.log(countries.push("Ethiopia"));
+    console.log(countries);
+  }
+}
+console.log(checkCountry(countries));
+
+//console.log(countries.includes("Ethiopia"));
+
+//6.Concatenate the following two variables and store it in a fullStack variable.
+
+const frontEnd = ["HTML", "CSS", "JS", "React", "Redux"];
+const backEnd = ["Node", "Express", "MongoDB"];
+const fullStack = frontEnd.concat(backEnd);
+
+console.log(fullStack);
+
+//Exercise: Level 3
+//The following is an array of 10 students ages:
+
+const ages5 = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+//Sort the array and find the min and max age
+//Find the median age(one middle item or two middle items divided by two)
+//Find the average age(all items divided by number of items)
+//Find the range of the ages5(max minus min)
+//Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array
+
+console.log(ages5.sort());
+//[19, 19, 20, 22, 24, 24, 24, 25, 25, 26]
+
+//console.log(ages5[0], ages5[ages5.length - 1], ages5[ages5.length - 2]);
+//console.log("middle", ages5[Math.ceil(ages5.length / 2 - 1)]);
+
+function printTheMiddlenum(arr3) {
+  let medianValue;
+  if (arr3.length % 2 === 0) {
+    let middleNum1 = ages5[Math.ceil(ages5.length / 2)];
+    let middleNum2 = ages5[Math.ceil(ages5.length / 2 - 1)];
+    medianValue = "median = " + (middleNum1 + middleNum2 / 2);
+  } else {
+    medianValue = ages5[Math.ceil(ages5.length / 2 - 1)];
+  }
+  return medianValue;
+}
+console.log(printTheMiddlenum(ages5));
+
+//let averageResult = findAvr(arr4) / ages5.length;
+
+function findAvr(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  let average = sum / arr.length;
+  return average;
+}
+
+console.log(findAvr(ages5));
+
+//let evenArr = [];
+//let oddArr = [];
+
+function countriesOddOrEven(arr5) {
+  if (arr5.length % 2 === 0) {
+    console.log("entered if loop");
+    console.log("even", countries[arr5.length / 2]);
+  } else {
+    console.log("not found ....");
+    console.log(countries.unshift("finland"));
+    console.log(countries);
+  }
+}
+countriesOddOrEven(countries);
+
+const bigCountriesList = [
+  "Afghanistan",
+  "Albania",
+  "Algeria",
+  "Andorra",
+  "Angola",
+  "Antigua and Barbuda",
+  "Argentina",
+  "Armenia",
+  "Australia",
+  "Austria",
+  "Azerbaijan",
+  "Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bhutan",
+  "Bolivia",
+  "Bosnia and Herzegovina",
+  "Botswana",
+  "Brazil",
+  "Brunei",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "Cambodia",
+  "Cameroon",
+  "Canada",
+  "Cape Verde",
+  "Central African Republic",
+  "Chad",
+  "Chile",
+  "China",
+  "Colombi",
+  "Comoros",
+  "Congo (Brazzaville)",
+  "Congo",
+  "Costa Rica",
+  "Cote d'Ivoire",
+  "Croatia",
+  "Cuba",
+  "Cyprus",
+  "Czech Republic",
+  "Denmark",
+  "Djibouti",
+  "Dominica",
+  "Dominican Republic",
+  "East Timor (Timor Timur)",
+  "Ecuador",
+  "Egypt",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Eritrea",
+  "Estonia",
+  "Ethiopia",
+  "Fiji",
+  "Finland",
+  "France",
+  "Gabon",
+  "Gambia, The",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Greece",
+  "Grenada",
+  "Guatemala",
+  "Guinea",
+  "Guinea-Bissau",
+  "Guyana",
+  "Haiti",
+  "Honduras",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Iraq",
+  "Ireland",
+  "Israel",
+  "Italy",
+  "Jamaica",
+  "Japan",
+  "Jordan",
+  "Kazakhstan",
+  "Kenya",
+  "Kiribati",
+  "Korea, North",
+  "Korea, South",
+  "Kuwait",
+  "Kyrgyzstan",
+  "Laos",
+  "Latvia",
+  "Lebanon",
+  "Lesotho",
+  "Liberia",
+  "Libya",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Macedonia",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Marshall Islands",
+  "Mauritania",
+  "Mauritius",
+  "Mexico",
+  "Micronesia",
+  "Moldova",
+  "Monaco",
+  "Mongolia",
+  "Morocco",
+  "Mozambique",
+  "Myanmar",
+  "Namibia",
+  "Nauru",
+  "Nepal",
+  "Netherlands",
+  "New Zealand",
+  "Nicaragua",
+  "Niger",
+  "Nigeria",
+  "Norway",
+  "Oman",
+  "Pakistan",
+  "Palau",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Poland",
+  "Portugal",
+  "Qatar",
+  "Romania",
+  "Russia",
+  "Rwanda",
+  "Saint Kitts and Nevis",
+  "Saint Lucia",
+  "Saint Vincent",
+  "Samoa",
+  "San Marino",
+  "Sao Tome and Principe",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia and Montenegro",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Slovakia",
+  "Slovenia",
+  "Solomon Islands",
+  "Somalia",
+  "South Africa",
+  "Spain",
+  "Sri Lanka",
+  "Sudan",
+  "Suriname",
+  "Swaziland",
+  "Sweden",
+  "Switzerland",
+  "Syria",
+  "Taiwan",
+  "Tajikistan",
+  "Tanzania",
+  "Thailand",
+  "Togo",
+  "Tonga",
+  "Trinidad and Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Tuvalu",
+  "Uganda",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom",
+  "United States",
+  "Uruguay",
+  "Uzbekistan",
+  "Vanuatu",
+  "Vatican City",
+  "Venezuela",
+  "Vietnam",
+  "Yemen",
+  "Zambia",
+  "Zimbabwe",
+];
+
+//console.log(bigCountriesList);
+let coutryAns = bigCountriesList[Math.ceil(bigCountriesList.length / 2 - 1)];
+console.log(coutryAns);
