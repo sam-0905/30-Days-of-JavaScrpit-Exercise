c704d1f6cda922c8e4a5cba4db895081e6ddf5e0
AllanSam <allansam0905@gmail.com>
2024-03-05 09:16:08 +0530

    Renamed for all files

--- 30Daysofjs/Datatypes.js
+++ 30Daysofjs/Datatypes.js
@@ -1,4 +1,4 @@
-/*
+/* 
 1.Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
 2.Print the string on the browser console using console.log()
 3.Print the length of the string on the browser console using console.log()
---
+++ 30Daysofjs/loops.js
@@ -0,0 +1,2 @@
+// can be also shorten like this, sum += num
+// after this we will use the shorter synthax(+=, -=, *=, /= etc)
