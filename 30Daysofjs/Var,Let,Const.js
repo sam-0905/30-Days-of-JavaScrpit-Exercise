b8f7a6343c097204e1fabb770d5e866425905a87
AllanSam <allansam0905@gmail.com>
2024-04-13 20:20:05 +0530

    var concepts completed

--- 30Daysofjs/Var,Let,Const.js
+++ 30Daysofjs/Var,Let,Const.js
@@ -21,3 +21,19 @@ var d = "aneena";
 d = "aneena"; // will give the same result.
 
 console.log(d); //aneena
+
+//iii) Hoisting in var
+//var b ;
+console.log(b); // we call before we declare the variable.
+var b = 10;
+//outpu > undefined - In this case Var is moving the declaration top of the program as we seen in the example and intialize it in the value of undefined.
+
+//iv) Var In case of block scope
+//HACK: var doesn't have  block scope.
+
+var greeter1 = "hi";
+
+if (true) {
+    var greeter1 = "say hello instead";
+}
+//output > say hello instead - this greeter1 gets overwitten by above greeter1 value.
