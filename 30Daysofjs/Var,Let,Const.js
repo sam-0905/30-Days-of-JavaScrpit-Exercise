8f1349c5d5744c0c6e28264336874563a0d5dc77
AllanSam <allansam0905@gmail.com>
2024-04-13 20:33:18 +0530

    completed let concepts

--- 30Daysofjs/Var,Let,Const.js
+++ 30Daysofjs/Var,Let,Const.js
@@ -30,6 +30,7 @@ var b = 10;
 
 //iv) Var In case of block scope
 //HACK: var doesn't have  block scope.
+//! It is one the bug incase of var.For that instead of var they use let.
 
 var greeter1 = "hi";
 
@@ -37,3 +38,35 @@ if (true) {
     var greeter1 = "say hello instead";
 }
 //output > say hello instead - this greeter1 gets overwitten by above greeter1 value.
+
+//------------------------------------------------------------------------------------------------
+
+//NOTE: let
+//? iIt won't pollute the value outside the block.
+let greeter2 = "hi";
+
+if (true) {
+    let hello = "say hello instead";
+    console.log(hello); // say hello instead
+}
+
+console.log(greeter2); // hi
+console.log(hello); //!It gives, Reference error : hello is not defined.
+
+//output > like we seen above- this greeter2 dosen't overwitten by above greeter2 value.
+
+//ii) let can also be "ReDeclare" the value with the same variable name.
+
+let e = "appu";
+e = "aneena"; // will give the same result.
+//let e = "aneena"; //!  It gives, syntax error :  Identifier e is already declared.
+
+console.log(e); //aneena
+
+//iii) Hoisting in let
+//let b ;
+console.log(f); //!It gives, Reference error : cannot access befor intialization.
+let f = 10;
+//outpu > undefined - In this case Var is moving the declaration top of the program as we seen in the example and intialize it in the value of undefined.
+
+//NOTE: CONST
