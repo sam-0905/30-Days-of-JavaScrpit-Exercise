c7af232975c3407d5494be318a5675ce17272644
AllanSam <allansam0905@gmail.com>
2024-04-15 20:06:30 +0530

    updated

--- 30Daysofjs/Var,Let,Const.js
+++ 30Daysofjs/Var,Let,Const.js
@@ -5,7 +5,6 @@
 var greeter = "hey hi"; // globally scoped.
 
 //i) var can also be functional scope:
-
 function myFunction() {
     var a = "hello";
     console.log(a); // it will works.
@@ -15,7 +14,6 @@ console.log(greeter); // we can call ,Bcz its globally declare.
 console.log(a); //! Reference error : a is not defined.
 
 //ii) var can be "ReDeclare" the value with the same variable name.
-
 var d = "appu";
 var d = "aneena";
 d = "aneena"; // will give the same result.
@@ -31,7 +29,6 @@ var b = 10;
 //iv) Var In case of block scope
 //HACK: var doesn't have  block scope.
 //! It is one the bug incase of var.For that instead of var they use let.
-
 var greeter1 = "hi";
 
 if (true) {
@@ -42,21 +39,19 @@ if (true) {
 //------------------------------------------------------------------------------------------------
 
 //NOTE: let
-//? iIt won't pollute the value outside the block.
+//? It won't pollute the value outside the block.
 let greeter2 = "hi";
 
 if (true) {
     let hello = "say hello instead";
     console.log(hello); // say hello instead
 }
-
 console.log(greeter2); // hi
 console.log(hello); //!It gives, Reference error : hello is not defined.
 
 //output > like we seen above- this greeter2 dosen't overwitten by above greeter2 value.
 
 //ii) let can also be "ReDeclare" the value with the same variable name.
-
 let e = "appu";
 e = "aneena"; // will give the same result.
 //let e = "aneena"; //!  It gives, syntax error :  Identifier e is already declared.
