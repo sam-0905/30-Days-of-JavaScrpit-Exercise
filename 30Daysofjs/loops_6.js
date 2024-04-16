87f39dc0785b8ce5bfa3d9d7841e67fdd026357e
AllanSam <allansam0905@gmail.com>
2024-04-16 19:17:07 +0530

    updated some changes in loop example

--- 30Daysofjs/loops_6.js
+++ 30Daysofjs/loops_6.js
@@ -20,72 +20,72 @@ Iterate 0 to n using for loop
 **/
 
 const countries = [
-  "US",
-  "Albania",
-  "Bolivia",
-  "Canada",
-  "Denmark",
-  "Ethiopia",
-  "Finland",
-  "Germany",
-  "Hungary",
-  "Ireland",
-  "Japan",
-  "Kenya",
+    "US",
+    "Albania",
+    "Bolivia",
+    "Canada",
+    "Denmark",
+    "Ethiopia",
+    "Finland",
+    "Germany",
+    "Hungary",
+    "Ireland",
+    "Japan",
+    "Kenya",
 ];
 
 // print/iterate 0 to 10 /Print all entries in array.
 function printAllElem(arr) {
-  const newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    newArr.push(arr[i]);
-  }
-  return newArr;
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i]);
+    }
+    return newArr;
 }
 console.log(printAllElem(countries));
 
 //Print all arr toUpperCase()
 function printAllElem2(arr) {
-  const newArr1 = [];
-  for (let i = 0; i < arr.length; i++) {
-    newArr1.push(arr[i].toUpperCase());
-  }
-  return newArr1;
+    const newArr1 = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr1.push(arr[i].toUpperCase());
+    }
+    return newArr1;
 }
 console.log(printAllElem2(countries));
 
 //print array is less than five charactrers
 function printAllElem3(arr) {
-  const newArr1 = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].length <= 5) {
-      newArr1.push(arr[i]);
+    const newArr1 = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length <= 5) {
+            newArr1.push(arr[i]);
+        }
     }
-  }
-  return newArr1;
+    return newArr1;
 }
 console.log(printAllElem3(countries));
 
 //NOTE: Reverse An array
 function printAllElem4(arr) {
-  //  const newArr1 = [];
-  //  for (let i = 0; i < arr.length; i++) {
-  //    newArr1.push(arr[i].reverse());
-  //!    newArr1.reverse();
-  //  }
-  //  return newArr1;
-  return arr.reverse();
+    //  const newArr1 = [];
+    //  for (let i = 0; i < arr.length; i++) {
+    //    newArr1.push(arr[i].reverse());
+    //!    newArr1.reverse();
+    //  }
+    //  return newArr1;
+    return arr.reverse();
 }
 
 console.log("Reversed:", printAllElem4(countries));
 
 const sentence = [
-  "Facebook",
-  "Google",
-  "Microsoft",
-  "Apple",
-  "IBM",
-  "Oracle and Amazon are big IT companies",
+    "Facebook",
+    "Google",
+    "Microsoft",
+    "Apple",
+    "IBM",
+    "Oracle and Amazon are big IT companies",
 ];
 //NOTE:
 //Reverse An array
@@ -104,8 +104,8 @@ console.log(number);
 let i = 0;
 const newArr = [];
 while (i < sentence.length) {
-  newArr.push(sentence[i]);
-  i++;
+    newArr.push(sentence[i]);
+    i++;
 }
 console.log("While loop....", newArr);
 
@@ -113,8 +113,8 @@ console.log("While loop....", newArr);
 i = 0;
 const newArr1 = [];
 while (i < sentence.length) {
-  newArr1.push(sentence[i].toUpperCase());
-  i++;
+    newArr1.push(sentence[i].toUpperCase());
+    i++;
 }
 console.log(newArr1);
 
@@ -124,9 +124,9 @@ console.log(newArr1);
 //For instance, a shorter way to write while (i != 0) is while (i):
 let i2 = 3;
 while (i2) {
-  // when i becomes 0, the condition becomes falsy, and the loop stops
-  console.log("From while loop..", i2);
-  i2--;
+    // when i becomes 0, the condition becomes falsy, and the loop stops
+    console.log("From while loop..", i2);
+    i2--;
 }
 
 //DOCS: do while loop
@@ -135,30 +135,32 @@ while (i2) {
 
 i = 0;
 do {
-  console.log("do while loop....", i);
-  i++;
+    console.log("do while loop....", i);
+    i++;
 } while (i <= 3);
 
-//DOCS: for in loop
+//DOCS:
+// ? for in loop
 //for in loop only for object.
 const user = {
-  name: "Pappu",
-  breed: "Golden",
-  age: "2years",
+    name: "Pappu",
+    breed: "Golden",
+    age: "2years",
 };
 for (key in user) {
-  console.log(key);
-  user[key] = user[key].toLowerCase();
+    console.log(key);
+    user[key] = user[key].toLowerCase();
 }
 console.log("object...", user);
 
-//DOCS: for of loop - Arrays,Strings,Maps
+//DOCS:
+// ? for of loop - Arrays,Strings,Maps
 //We use for of loop for arrays. It is very hand way to iterate through an array if we are not interested in the index of each element in the array.
 
 const countries1 = ["Finland", "Sweden", "Norway", "Denmark", "Iceland"];
 const newArr12 = [];
 for (const country of countries1) {
-  newArr12.push(country.toUpperCase());
+    newArr12.push(country.toUpperCase());
 }
 console.log(newArr12);
 
@@ -166,20 +168,20 @@ console.log(newArr12);
 //Break is used to interrupt a loop.
 
 for (let i = 0; i <= 5; i++) {
-  if (i == 3) {
-    break;
-  }
-  console.log("break", i);
+    if (i == 3) {
+        break;
+    }
+    console.log("break", i);
 }
 
 //!continue
 //We use the keyword continue to skip a certain iterations.
 
 for (let i = 0; i <= 5; i++) {
-  if (i == 3) {
-    continue;
-  }
-  console.log("continue", i);
+    if (i == 3) {
+        continue;
+    }
+    console.log("continue", i);
 }
 
 //?
@@ -197,9 +199,9 @@ for (let i = 0; i <= 5; i++) {
 //10 x 10 = 100
 
 function multiplyAllElem() {
-  for (let i = 0; i <= 10; i++) {
-    console.log(`${i} * ${i} = ${i * i}`);
-  }
+    for (let i = 0; i <= 10; i++) {
+        console.log(`${i} * ${i} = ${i * i}`);
+    }
 }
 
 multiplyAllElem();
@@ -215,10 +217,10 @@ multiplyAllElem();
 //    #######
 
 function PrintTheFollParten() {
-  console.log("printing .........");
-  for (let i = 0; i < 8; i++) {
-    console.log("#".repeat(i));
-  }
+    console.log("printing .........");
+    for (let i = 0; i < 8; i++) {
+        console.log("#".repeat(i));
+    }
 }
 
 PrintTheFollParten();
@@ -239,11 +241,11 @@ PrintTheFollParten();
 // 10   100   1000
 
 function PrintSquareCubeElem() {
-  console.log("i", "i^2", "i^3 .......");
+    console.log("i", "i^2", "i^3 .......");
 
-  for (let i = 0; i <= 10; i++) {
-    console.log(`${i} ${i * i} ${i * i * i}`);
-  }
+    for (let i = 0; i <= 10; i++) {
+        console.log(`${i} ${i * i} ${i * i * i}`);
+    }
 }
 
 PrintSquareCubeElem();
@@ -251,20 +253,20 @@ PrintSquareCubeElem();
 //? Use for loop to iterate from 0 to 100 and print only even numbers
 
 function PrintEvenNum() {
-  const evenArr = [];
-  const oddArr = [];
-  for (let i = 0; i <= 100; i++) {
-    //console.log(i % 2 === 0);
-    if (i % 2 === 0) {
-      evenArr.push(i);
-    } else {
-      oddArr.push(i);
+    const evenArr = [];
+    const oddArr = [];
+    for (let i = 0; i <= 100; i++) {
+        //console.log(i % 2 === 0);
+        if (i % 2 === 0) {
+            evenArr.push(i);
+        } else {
+            oddArr.push(i);
+        }
+        //! If we console here it print each and every number in single time & not in an array
+        //evenArr.push(i);
+        //console.log(evenArr);
     }
-    //! If we console here it print each and every number in single time & not in an array
-    //evenArr.push(i);
-    //console.log(evenArr);
-  }
-  return evenArr;
+    return evenArr;
 }
 
 console.log("even", PrintEvenNum());
@@ -272,13 +274,13 @@ console.log("even", PrintEvenNum());
 //? Use for loop to iterate from 0 to 100 and print only odd numbers
 
 const PrintOddNum = () => {
-  const oddArr = [];
-  for (let i = 0; i <= 100; i++) {
-    if (i % 2 !== 0) {
-      oddArr.push(i);
+    const oddArr = [];
+    for (let i = 0; i <= 100; i++) {
+        if (i % 2 !== 0) {
+            oddArr.push(i);
+        }
     }
-  }
-  return oddArr;
+    return oddArr;
 };
 
 console.log("odd", PrintOddNum());
@@ -294,23 +296,23 @@ In this JavaScript code:
 **/
 
 function printPrimeNum() {
-  const newArr = [];
+    const newArr = [];
 
-  for (let num = 2; num <= 100; num++) {
-    let isPrime = true;
+    for (let num = 2; num <= 100; num++) {
+        let isPrime = true;
 
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-      if (num % i === 0) {
-        isPrime = false;
-        break;
-      }
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            //console.log(num);
+            newArr.push(num);
+        }
     }
-    if (isPrime) {
-      //console.log(num);
-      newArr.push(num);
-    }
-  }
-  return newArr;
+    return newArr;
 }
 
 console.log("Prime Numbers..", printPrimeNum());
@@ -318,26 +320,26 @@ console.log("Prime Numbers..", printPrimeNum());
 //?The sum of all numbers from 0 to 100 is 5050.
 
 function sumOfAllNum() {
-  let sum = 0;
-  for (let i = 0; i <= 100; i++) {
-    sum += i;
-  }
-  //console.log(sum);
-  return sum;
+    let sum = 0;
+    for (let i = 0; i <= 100; i++) {
+        sum += i;
+    }
+    //console.log(sum);
+    return sum;
 }
 
 console.log("sum = ", sumOfAllNum());
 
 //?The sum of all evens from 0 to 100 is 2550.
 function sumOfAllEvenNum() {
-  let sum = 0;
-  for (let i = 0; i <= 100; i++) {
-    if (i % 2 === 0) {
-      sum += i;
+    let sum = 0;
+    for (let i = 0; i <= 100; i++) {
+        if (i % 2 === 0) {
+            sum += i;
+        }
     }
-  }
-  //console.log(sum);
-  return sum;
+    //console.log(sum);
+    return sum;
 }
 
 console.log("sum of Even numbers", sumOfAllEvenNum());
@@ -345,14 +347,14 @@ console.log("sum of Even numbers", sumOfAllEvenNum());
 //?And the sum of all odds from 0 to 100 is 2500.
 
 function sumOfAllOddNum() {
-  let sum = 0;
-  for (let i = 0; i <= 100; i++) {
-    if (i % 2 !== 0) {
-      sum += i;
+    let sum = 0;
+    for (let i = 0; i <= 100; i++) {
+        if (i % 2 !== 0) {
+            sum += i;
+        }
     }
-  }
-  //console.log(sum);
-  return sum;
+    //console.log(sum);
+    return sum;
 }
 
 console.log("sum of odd numbers", sumOfAllOddNum());
@@ -360,36 +362,36 @@ console.log("sum of odd numbers", sumOfAllOddNum());
 //?Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds. Print sum of evens and sum of odds as array [2550, 2500]
 
 function sumOfAllEvenAndOddNum() {
-  let evenSum = 0;
-  let oddSum = 0;
+    let evenSum = 0;
+    let oddSum = 0;
 
-  for (let i = 0; i <= 100; i++) {
-    if (i % 2 === 0) {
-      evenSum += i;
-    } else {
-      oddSum += i;
+    for (let i = 0; i <= 100; i++) {
+        if (i % 2 === 0) {
+            evenSum += i;
+        } else {
+            oddSum += i;
+        }
     }
-  }
-  return [evenSum, oddSum];
+    return [evenSum, oddSum];
 
-  //! In case of objects
-  //DOCS: If the key and value of the object is same then we could use  { evenSum, oddSum }
-  //return { evenSum, oddSum };
+    //! In case of objects
+    //DOCS: If the key and value of the object is same then we could use  { evenSum, oddSum }
+    //return { evenSum, oddSum };
 
-  //DOCS: If the key and value of the object is different then we could use  { even: evenSum, odd: oddSum }
-  return { evenSumOfNums: evenSum, oddSumOfNums: oddSum };
+    //DOCS: If the key and value of the object is different then we could use  { even: evenSum, odd: oddSum }
+    return { evenSumOfNums: evenSum, oddSumOfNums: oddSum };
 }
 
 console.log("Even and Odd sum:", sumOfAllEvenAndOddNum());
 
 //? Develop a small script which generate array of 5 random numbers
 function generateRandomArray(length, min, max) {
-  let randomArray = [];
-  for (let i = 0; i < length; i++) {
-    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    randomArray.push(randomNumber);
-  }
-  return randomArray;
+    let randomArray = [];
+    for (let i = 0; i < length; i++) {
+        let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        randomArray.push(randomNumber);
+    }
+    return randomArray;
 }
 
 const arrayLength = 5;
@@ -405,13 +407,13 @@ const randomId = generateRandomId();
 console.log("Generated Random ID:", randomId);
 
 function generateRandomId() {
-  let characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let id = "";
-  for (let i = 0; i < 6; i++) {
-    id += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return id;
+    let characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let id = "";
+    for (let i = 0; i < 6; i++) {
+        id += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return id;
 }
 
 //? Develop a small script which generate array of 5 random numbers and the numbers must be unique
@@ -421,39 +423,39 @@ const minValue3 = 1;
 const maxValue3 = 100;
 
 const randomArray3 = generateUniqueRandomArray(
-  arrayLength3,
-  minValue3,
-  maxValue3
+    arrayLength3,
+    minValue3,
+    maxValue3
 );
 console.log("Generated Unique Random Array:", randomArray3);
 
 function generateUniqueRandomArray(length, min, max) {
-  if (max - min + 1 < length) {
-    throw new Error(
-      "Cannot generate unique numbers with the given range and length"
-    );
-  }
-
-  let randomArray = [];
-  while (randomArray.length < length) {
-    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    if (!randomArray.includes(randomNumber)) {
-      randomArray.push(randomNumber);
+    if (max - min + 1 < length) {
+        throw new Error(
+            "Cannot generate unique numbers with the given range and length"
+        );
     }
-  }
-  return randomArray;
+
+    let randomArray = [];
+    while (randomArray.length < length) {
+        let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        if (!randomArray.includes(randomNumber)) {
+            randomArray.push(randomNumber);
+        }
+    }
+    return randomArray;
 }
 
 //?Develop a small script which generate any number of characters random id:
 
 function generateRandomId2(length) {
-  let characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let id = "";
-  for (let i = 0; i < length; i++) {
-    id += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return id;
+    let characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let id = "";
+    for (let i = 0; i < length; i++) {
+        id += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return id;
 }
 
 const numberOfCharacters = 20; // Change this to generate IDs with different lengths
@@ -463,10 +465,10 @@ console.log("Generated Random ID:", randomId2);
 // ?Write a script which generates a random rgb color number.
 
 function generateRandomRGBColor() {
-  const red = Math.floor(Math.random() * 256); // Random integer between 0 and 255
-  const green = Math.floor(Math.random() * 256); // Random integer between 0 and 255
-  const blue = Math.floor(Math.random() * 256); // Random integer between 0 and 255
-  return `rgb(${red}, ${green}, ${blue})`;
+    const red = Math.floor(Math.random() * 256); // Random integer between 0 and 255
+    const green = Math.floor(Math.random() * 256); // Random integer between 0 and 255
+    const blue = Math.floor(Math.random() * 256); // Random integer between 0 and 255
+    return `rgb(${red}, ${green}, ${blue})`;
 }
 
 const randomColor = generateRandomRGBColor();
