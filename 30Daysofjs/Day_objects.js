195b680d2d2f67f649b9b3ec9299670f536f21df
AllanSam <allansam0905@gmail.com>
2024-04-16 08:27:18 +0530

    objescts concepts updated

--- 30Daysofjs/Day_objects.js
+++ 30Daysofjs/Day_objects.js
@@ -24,3 +24,39 @@ function letsLearnScope() {
 }
 letsLearnScope();
 console.log(c, d); // JavaScript 10, accessible
+
+const person = {
+    firstName: "Asabeneh",
+    lastName: "Yetayeh",
+    age: 250,
+    country: "Finland",
+    city: "Helsinki",
+    skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React",
+        "Node",
+        "MongoDB",
+        "Python",
+        "D3.js",
+    ],
+    isMarried: true,
+};
+console.log(person);
+
+// accessing values using .
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
+console.log(person.location); // undefined
+
+// value can be accessed using square bracket and key name
+console.log(person["firstName"]);
+console.log(person["lastName"]);
+console.log(person["age"]);
+console.log(person["age"]);
+console.log(person["location"]); // undefined
+
+// for instance to access the phone number we only use the square bracket method
+console.log(person["phone number"]);
