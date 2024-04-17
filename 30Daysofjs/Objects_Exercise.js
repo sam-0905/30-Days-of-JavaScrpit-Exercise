6ad90dc4da3d32185fcc024b1cdcd9bccd2f9224
AllanSam <allansam0905@gmail.com>
2024-04-17 13:50:51 +0530

    completed execise level 1

---
+++ 30Daysofjs/Objects_Exercise.js
@@ -0,0 +1,27 @@
+/*
+Exercises: Level 1
+1.Create an empty object called dog/
+2.Print the the dog object on the console.
+3.Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof.
+4.Get name, legs, color, age and bark value from the dog object.
+5.Set new properties the dog object: breed, getDogInfo.
+**/
+
+const dog = {};
+dog.name = "Oscar";
+dog.legs = "Four";
+dog.color = "Golden";
+dog.age = 2;
+dog.bark = "yes";
+
+console.log(dog);
+
+const dog2 = {
+    name: "ceasear",
+    legs: "Fur",
+    color: "Black with White",
+    age: 10,
+    bark: "yes",
+};
+
+console.log(dog2);
